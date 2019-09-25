baebd77166ab7920c30a2730c578b99555ad5e42
whale-52hz <liuliangswust@qq.com>
2019-09-25 22:14:02 +0800

    更新服务端-优化

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,6 @@ var router = express.Router();
 
 var sql_operation = require('./db/dbOperation/operation');
 
-//测试
-// var MysqlPool = require('./mysql/mysql_pool.js');
-// var mysqlPool = new MysqlPool();
-// var pool = mysqlPool.getPool();
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -22,19 +16,9 @@ router.get('/add', function(req, res, next){
 
 router.get('/query', function(req, res, next){
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
   sql_operation.query(req, res, next);
 
-  // var {url:url,query} = urlModule.parse(req.url,true);
-  //res.setHeader("Access-Control-Allow-Origin", "*");
-  // if(query.name === 'start_geohash' || query.name === 'end_geohash'){
-  //   pool.query(`SELECT * FROM db_traffic.${query.name}`,(err,rows)=>{
-  //     if(err) throw err;
-  //     //console.log(rows);
-  //     res.send(rows);
-  //   });
-  // }
-  // else
-  //   res.end('Collection Not Found!');
 });
 
 router.get('/update', function(req, res, next){
